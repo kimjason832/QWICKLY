462b6c3b05ac93fe3289fc1fd42008fbd0573fbc
kimjason832 <kimjason.832@gmail.com>
2023-08-10 15:06:43 -0700

    Initial UI Design

---
+++ app/src/main/java/com/example/qwickly/qr_code_screen.java
@@ -0,0 +1,14 @@
+package com.example.qwickly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class qr_code_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_code_screen);
+    }
+}
---
+++ app/src/main/java/com/example/qwickly/status_screen.java
@@ -0,0 +1,14 @@
+package com.example.qwickly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class status_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.status_screen);
+    }
+}

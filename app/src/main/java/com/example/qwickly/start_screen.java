00cf151eac8a62796696ee5fd2aea95798c68b68
kimjason832 <kimjason.832@gmail.com>
2023-08-14 13:30:58 -0700

    Sign up/in screen Frame Layouts

--- app/src/main/java/com/example/qwickly/start_screen.java
+++ app/src/main/java/com/example/qwickly/start_screen.java
@@ -2,13 +2,31 @@ package com.example.qwickly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class start_screen extends AppCompatActivity {
+
+    private Button startScreenTosignInScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+
+        //changes screen from Start Screen to Sign In Screen
+        startScreenTosignInScreen = findViewById(R.id.startScreen_signInButton);
+        startScreenTosignInScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(start_screen.this, signIn_screen.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }

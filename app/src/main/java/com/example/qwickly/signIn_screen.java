f3ae939f6023d7a113fb9009f6784aa052b91bea
kimjason832 <kimjason.832@gmail.com>
2023-08-15 14:35:18 -0700

    Sign In/Up Screen Transfer + Menu Bar Layout Changes

--- app/src/main/java/com/example/qwickly/signIn_screen.java
+++ app/src/main/java/com/example/qwickly/signIn_screen.java
@@ -2,13 +2,40 @@ package com.example.qwickly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class signIn_screen extends AppCompatActivity {
+
+    private Button signInScreenTosignUpScreen;
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_screen);
+
+        //changes screen from Sign In Screen to Sign Up Screen
+        signInScreenTosignUpScreen = (Button) findViewById(R.id.signInScreen_signUpButton);
+        signInScreenTosignUpScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signIn_screen.this, sign_up_screen.class);
+                startActivity(intent);
+            }
+        });
+
+        //changes screen from Sign In Screen to Home Screen
+        signInButton = (Button) findViewById(R.id.signInScreen_signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signIn_screen.this, menu_screen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/qwickly/sign_up_screen.java
+++ app/src/main/java/com/example/qwickly/sign_up_screen.java
@@ -2,13 +2,29 @@ package com.example.qwickly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class sign_up_screen extends AppCompatActivity {
+
+    private Button signUpScreenTosignInScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_screen);
+
+        //changes screen from Sign In Screen to Sign Up Screen
+        signUpScreenTosignInScreen = (Button) findViewById(R.id.signUpScreen_signInButton);
+        signUpScreenTosignInScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sign_up_screen.this, signIn_screen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

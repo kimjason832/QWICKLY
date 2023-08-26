1c549026ca7ee79a772645102706d7bf0922751b
kimjason832 <kimjason.832@gmail.com>
2023-08-25 17:04:03 -0700

    Sign Up w/ user data in cloud firestore

--- app/src/main/java/com/example/qwickly/sign_up_screen.java
+++ app/src/main/java/com/example/qwickly/sign_up_screen.java
@@ -4,19 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class sign_up_screen extends AppCompatActivity {
 
@@ -25,6 +33,8 @@ public class sign_up_screen extends AppCompatActivity {
     Button signUpButton;
     ProgressBar progressBar;
     FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     public void onStart() {
@@ -43,6 +53,7 @@ public class sign_up_screen extends AppCompatActivity {
         setContentView(R.layout.sign_up_screen);
 
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         signUpScreenTosignInScreen = (Button) findViewById(R.id.signUpScreen_signInButton);
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
@@ -74,6 +85,21 @@ public class sign_up_screen extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     Toast.makeText(sign_up_screen.this, "Account created.",
                                             Toast.LENGTH_SHORT).show();
+                                    userID = mAuth.getCurrentUser().getUid();
+                                    Map<String,Object> user = new HashMap<>();
+                                    user.put("Email", email);
+                                    user.put("Password", password);
+                                    user.put("Hours", 0);
+//                                    fStore.collection("Users")
+//                                            .add(user);
+                                    DocumentReference documentReference = fStore.collection("Users").document(userID);
+                                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+
+                                        }
+                                    });
+
                                     Intent intent = new Intent(getApplicationContext(), signIn_screen.class);
                                     startActivity(intent);
                                     finish();
